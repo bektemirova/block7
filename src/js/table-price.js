da9ddaaf1b26bf942645bf8a1d019b3b655e06fc
bektemirova <jibek260886@gmail.com>
2022-12-09 23:22:32 +0300

    fixed 3,16

--- src/js/table-price.js
+++ src/js/table-price.js
@@ -13,33 +13,50 @@ export const createTable = (key, title) => {
   let priceRow = document.createElement("div");
   priceRow.classList.add("swiper-slide");
   priceRow.classList.add("price__row");
-  priceRow.innerHTML = `
-                        <div class="price__cell" data-title="Ремонтные услуги">
-                                        <span>${ key[0] }</span>
-                                        <span>${ title[0] }</span>
-                           </div>
-                         <div class="price__cell" data-title="Цена">
-                              <span>${ key[1] }</span>
-                               <span> ${ title[1] }</span>
-                            </div>
-                            <div class="price__cell" data-title="Срок">
-                              <span>${ key[2] }</span>
-                                  <div class="price__cell-btn">
-                                    <span>${ title[2] }</span>
-                                      <button
-                                            type="button"
-                                            class="price__btn"
-                                            aria-label="Заказать"
-                                        >
-                                            Заказать
-                                          </button>
-                                   </div>
-                           </div>
-  `;
-  return priceRow;
+
+  let priceCell = document.createElement("div")
+  priceCell.classList.add("price__cell")
+  let span1 = document.createElement("span")
+  let span2 = document.createElement("span")
+  span1.textContent = key[0]
+  span2.textContent = title[0]
+  priceCell.appendChild(span1)
+  priceCell.appendChild(span2)
+
+  let priceCell2 = document.createElement("div")
+  priceCell2.classList.add("price__cell")
+  let span3 = document.createElement("span")
+  let span4 = document.createElement("span")
+  span3.textContent = key[1]
+  span4.textContent = title[1]
+  priceCell2.appendChild(span3)
+  priceCell2.appendChild(span4)
+
+  let priceCell3 = document.createElement("div")
+  priceCell3.classList.add("price__cell")
+  let span5 = document.createElement("span")
+  span5.textContent = key[2]
+  let btnDiv = document.createElement("div")
+  btnDiv.classList.add("price__cell-btn")
+  let span6 = document.createElement("span")
+  span6.textContent = title[2]
+  btnDiv.appendChild(span6)
+  let btn = document.createElement("button")
+  btn.classList.add("price__btn")
+  btn.textContent = "Заказать"
+  btnDiv.appendChild(btn)
+  priceCell3.appendChild(btnDiv)
+
+  priceRow.appendChild(priceCell)
+  priceRow.appendChild(priceCell2)
+  priceRow.appendChild(priceCell3)
+
+  return priceRow
 };
 
 for (let el of data) {
   let item = createTable(keys, el);
   priceBox.appendChild(item);
 }
+
+
